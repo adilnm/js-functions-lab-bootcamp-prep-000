b0f0927538301481102acd66e072f62574d594c7
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-07 03:15:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
-function happyHolidaysTo(name){
+function happyHolidayTo(holiday,name){
   return `Happy holidays, ${name}!`
 }
